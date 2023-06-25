db3ba0907d4b3b6dde85b4e9271c80bd937798db
Abdul Rehman Ikram <abdul@bioinformatics.pk>
2023-06-26 02:17:58 +0500

    Add files via upload

---
+++ getData.R
@@ -0,0 +1,18 @@
+# script to get data from airway package
+
+setwd("C:/Users/92318/Desktop/R-Course/DESEQ2/")
+
+library(airway)
+
+data(airway)
+airway
+
+sample_info <- as.data.frame(colData(airway))
+sample_info <- sample_info[,c(2,3)]
+sample_info$dex <- gsub('trt', 'treated', sample_info$dex)
+sample_info$dex <- gsub('untrt', 'untreated', sample_info$dex)
+names(sample_info) <- c('cellLine', 'dexamethasone')
+write.table(sample_info, file = "sample_info.csv", sep = ',', col.names = T, row.names = T, quote = F)
+
+countsData <- assay(airway)
+write.table(countsData, file = "counts_data.csv", sep = ',', col.names = T, row.names = T, quote = F)
